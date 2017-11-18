9f84fb93283f2d5134f3f2e05bef1c39277dc2b1
yuki3738 <y.minamiya@gmail.com>
2017-11-18 20:01:33 +0900

    Change products_controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,6 +5,11 @@ class ProductsController < ApplicationController
   # GET /products.json
   def index
     @products = Product.all
+    respond_to do |format|
+      format.html
+      format.json { render json: @products }
+    end
+
   end
 
   # GET /products/1
