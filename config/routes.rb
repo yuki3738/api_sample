d60325f9337b19d2de989173132d5480174ac6cd
yuki3738 <y.minamiya@gmail.com>
2017-11-19 13:48:28 +0900

    Change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  namespace :api, { format: 'jason'} do
+    resources :products
+  end
+
   resources :products
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'products#index'
 end
